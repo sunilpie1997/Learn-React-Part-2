5dada3b7c580b4e3be52a3c0756c8eee9cfec247
sunilpie <sunilpie1997@gmail.com>
2020-09-23 18:54:53 +0530

    6.List and Keys (sol. to error in previous commit)

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -8,17 +8,21 @@ function ListRender() {
     /* other approach */
     const nameList=names.map(name=><h2>{name}</h2>)
 
+    /* add 'id' to make each entry unique  */
     const personList=[{
+        id:1,
         name:"sunil",
         age:23,
         skill:"react"
     },
-    {
+    {   
+        id:2,
         name:"bandia",
         age:22,
         skill:"business"
     },
     {
+        id:3,
         name:"raju",
         age:22,
         skill:"sales"
@@ -31,11 +35,16 @@ function ListRender() {
                 //names.map(name=><h2>{name}</h2>)
                 //nameList
 
+                /*
                 personList.map(person=><Person person={person}></Person>)
-                /* this usage throws following error in console:
+                 this usage throws following error in console:
 
                     Each child in a list should have a unique "key" prop
                 */
+
+                /* solution for above error */
+                personList.map(person=><Person key={person.id} person={person}></Person>)
+                
             }
         </div>
 
--- src/components/Person.js
+++ src/components/Person.js
@@ -2,11 +2,29 @@ import React from 'react'
 
 function Person({person}) {
 
+    
+    return (
+                <h3>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h3>                     
+        
+            )
+}
+
+/*
+
+BELOW CODE throws error as
+'key' is a reserved keyword required by React to uniquely identify each part of list as it uses virtual DOM.
+
+Further explanation is in notes
+
+function Person({person,key}) {
+
     return (
         
-                <h3>I am {person.name}. I am {person.age} years old. I know {person.skill}. </h3>       
-        
+                // <h3>{key} I am {person.name}. I am {person.age} years old. I know {person.skill}. </h3> 
+                     
     )
 }
+*/
+
 
 export default Person
